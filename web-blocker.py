2be02bf69ca253b8ba3cbc72d5a8608c48e69d9b
mgjean <p.jean90@gmail.com>
2018-09-10 22:17:47 +0800

    schedule task

--- web-blocker.py
+++ web-blocker.py
@@ -1,3 +1,11 @@
-host_path = r"C:\Windows\System32\drivers\etc\hosts"
-redirect = "127.0.0.1"
-websites_list = ["www.facebook.com","sg.yahoo.com"]
+# Task Scheduler Library
+# Create Task 
+# General Tab => (give a name) => "Run with highest privileges" - Check
+# Triggers Tab => New => At Startup
+# Actions Tab => New => Start A Program => Browse(program)
+# Conditions Tab => on AC Power (Uncheck)
+
+
+host_file_path = r"C:\Windows\System32\drivers\etc\hosts"
+local_page = "127.0.0.1"
+address_list = ["sg.yahoo.com","yahoo.com","www.yahoo.com"]
