346c4cdf668260cc103423b536e4c60f18191542
mgjean <p.jean90@gmail.com>
2018-09-16 15:36:03 +0800

    samples

--- decorators-exercises.py
+++ decorators-exercises.py
@@ -1,4 +1,4 @@
-from decorators import do_twice,timer
+from decorators import do_twice,timer,debug
 
 
 @do_twice
@@ -14,4 +14,13 @@ def run_timer(times):
 	for i in range(times):
 		sum([i**2 for i in range(1000)])
 
-run_timer(100)
+#run_timer(100)
+
+@debug
+def make_greeting(name, age=None):
+	if age is None:
+		return f"Hola {name}"
+	else:
+		return f"{name} is {age} years old!"
+
+make_greeting("mgj")
