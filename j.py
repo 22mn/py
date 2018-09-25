3da5b4dbae23b06dcd516cab66a276016b461cff
mgjean <p.jean90@gmail.com>
2018-09-25 22:23:22 +0800

    j with req

--- j.py
+++ j.py
@@ -1,10 +1,18 @@
-import json
+import requests,json
+"""
+
 data = {"president":
 			{"name":"Zaphod Beeblebrox",
 			 "species":"Betelgeusian"}}
-"""
+#write
 with open("data_file.json","w") as write_file:
 	json.dump(data,write_file)
-"""
-
 json_string = json.dumps(data)
+
+#read
+with open("data_file.json","r") as read_file:
+	data = json.load(read_file)
+data["president"]
+"""
+response = requests.get("https://jsonplaceholder.typicode.com/todos")
+todos = json.loads(response.text)
