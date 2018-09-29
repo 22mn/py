fe5ff822301549b21ba05e26446ac54a496c0bb5
mgjean <p.jean90@gmail.com>
2018-09-29 17:43:42 +0800

    programming python 4edition ebook exercises

---
+++ ebook-tutorials/initdata.py
@@ -0,0 +1,18 @@
+
+# initialize data to be stored in files, pickles, shelves
+
+# records
+bob = {"name": "Bob Smith", "age": 22, "pay": 50000, "job": "dev"}
+sue = {"name": "Sue Jones", "age": 21, "pay": 45000, "job": "ana"}
+tom = {"name": "Tom", "age": 50, "pay": 60000, "job": "mgr"}
+
+
+# database
+db = {}
+db["bob"] = bob
+db["sue"] = sue
+db["tom"] = tom
+
+if __name__ == '__main__':
+	for key in db:
+		print(key, "=>\n ", db[key])
