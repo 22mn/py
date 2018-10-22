68287e378094dab0f3792339e9941d2d72283f4d
mgjean <p.jean90@gmail.com>
2018-10-22 22:28:29 +0800

    gui3c

--- ebook-tutorials/part-3/chapter-7/gui1c.py
+++ ebook-tutorials/part-3/chapter-7/gui1c.py
@@ -1,6 +1,13 @@
 from tkinter import *
+
+def quit(name):
+	print("%s quit!" %name)
+	root.destroy()
+
 root = Tk()
-widget = Label(root)
-widget.config(text="Hello GUI world")
-widget.pack(side=TOP, expand=YES, fill=BOTH)
 root.title("Hola")
+widget = Button(root, text="Hello GUI world", command=(lambda : quit("mgj")))
+# use lambda to prevent invocation on button construction "command= quit('mgj')"
+# another way (lambda: print("mgj quit!") or root.destroy())
+widget.pack(side=TOP, expand=YES, fill=X)
+root.mainloop()
