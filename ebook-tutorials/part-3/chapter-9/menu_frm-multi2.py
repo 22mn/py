57aaf0f6c13c6dd5628a377af18afdff087520e9
mgjean <p.jean90@gmail.com>
2018-11-25 10:54:03 +0800

    multi

---
+++ ebook-tutorials/part-3/chapter-9/menu_frm-multi2.py
@@ -0,0 +1,12 @@
+from menu_frm import makemenu
+from tkinter import *
+
+root = Tk()
+for i in range(3):
+	frm = Frame()
+	menu = makemenu(frm)
+	menu.config(bd=2, relief=RAISED)
+	frm.pack(expand=YES, fill=BOTH)
+	Label(frm, bg="black", height=5, width=25).pack(expand=25, fill=BOTH)
+Button(root, text="Bye", command= root.destroy).pack()
+root.mainloop()
