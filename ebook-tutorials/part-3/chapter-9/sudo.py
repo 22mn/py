9182b59818fdefb962138f418d6eef5065b74575
mgjean <p.jean90@gmail.com>
2018-12-16 21:31:50 +0800

    (sudo) cell callback

--- ebook-tutorials/part-3/chapter-9/sudo.py
+++ ebook-tutorials/part-3/chapter-9/sudo.py
@@ -11,21 +11,31 @@ ValueDict = {}
 for i in range(81):
 	ValueDict[str(i)] = StringVar()
 
+userSteps = {}
+memory = []
+toUndo = []
+toRedo = []
+
 # trace callback 
 def hola(*args):
+	global memory
 	# args[0] StringVar()
 	cellnum = args[0][6:]
 	# input value
 	value = ValueDict[str(cellnum)].get()
 	
 	# check digit
-	if value.isdigit() and value != "0":
+	if value.isdigit() and value != "0" and len(value)==1:
 		# allow only one digit 
 		ValueDict[str(cellnum)].set(value[:1])
-		value = ValueDict[str(cellnum)].get()
-
 		# cursor blink offtime after input
 		allentries[int(cellnum)].config(insertofftime=100000)
+
+		# process to trace user steps
+		memory.append(cellnum)
+		toUndo.append(cellnum)
+		userSteps[cellnum] = value
+
 		print(" cell no: ", cellnum )
 		print(" input value: ", value)
 	else:
@@ -34,6 +44,11 @@ def hola(*args):
 count = 0
 
 allentries = []
+
+
+def hello(args):
+	print("hello")
+	print(args.widget)
 
 for row in range(rows):
 	for column in range(columns):
@@ -46,9 +61,48 @@ for row in range(rows):
 		# cursor size and blink time
 		ent.config(insertontime=700, insertwidth="0.2mm")
 
+		#***Test Validate***
+		# callback one time only (not using)
+		#ent.config(validate="focus", validatecommand=lambda ent=ent: hello(ent))
+
+		# ***Bind***
+		# call everytime focus-in (considering)
+		ent.bind("<FocusIn>",hello)
+
 		ent.grid(row=row,column=column,sticky=NSEW)
-		ValueDict[str(count)].trace("w", hola)    # "write" mode trace callback
+		
+		# "write" mode trace callback
+		ValueDict[str(count)].trace("w", hola)
+
 		allentries.append(ent)
 		count +=1
 #allentries = frame.winfo_children()
+
+def undo():
+	global toUndo,toRedo, memory
+	if toUndo:
+		toRedo = memory[len(toUndo)-1 : len(memory)]
+		currentEntry = toUndo[-1]
+		toUndo = toUndo[:-1]
+		ValueDict[currentEntry].set("")
+	else:
+		print("Not input yet!")
+
+undoButton = Button(frame, text="Undo", command=undo)
+undoButton.grid(sticky=NSEW)
+
+
+def redo():
+	global toRedo,toUndo,memory
+	if toRedo:
+		currentEntry = toRedo[0]
+		toRedo = toRedo[1:]
+		ValueDict[currentEntry].set(userSteps[currentEntry])
+	else:
+		print("Here is your last moved!")
+		
+
+redoButton = Button(frame, text="Redo", command=redo)
+redoButton.grid(sticky=NSEW)
+
 root.mainloop()
